7308b101886bb70126d78eeb8d80cc75aa6b8505
nezel <nezelskaazarina@gmail.com>
2023-10-16 02:19:07 +0300

    /

--- lab_15/lab_15.py
+++ lab_15/lab_15.py
@@ -1,38 +1,35 @@
 # Задача о раскраске графа
 
-# Создаем граф в виде списка смежности
+# Создаем граф в виде словаря, где ключи - вершины, а значения - их соседи
 graph = {
     1: [2, 3],
-    2: [1, 3, 4],
-    3: [1, 2, 4],
+    2: [1, 4, 5],
+    3: [1, 4],
     4: [2, 3, 5],
-    5: [4]
+    5: [2, 4],
+    6: []
 }
 
-# Функция для раскраски графа
-def greedy_coloring(graph):
-    color_map = {}  # Словарь для отображения вершины в цвет
+# Создаем словарь для хранения цветов вершин
+coloring = {}
 
-    for node in graph:
-        neighbor_colors = set()  # Множество цветов соседей текущей вершины
+# Упорядочиваем вершины по невозрастанию степени
+nodes_sorted_by_degree = sorted(graph.keys(), key=lambda x: -len(graph[x]))
 
-        # Проверяем цвета соседей
-        for neighbor in graph[node]:
-            if neighbor in color_map:
-                neighbor_colors.add(color_map[neighbor])
+# Начинаем окраску с первой вершины
+coloring[nodes_sorted_by_degree[0]] = 1
 
-        # Находим первый доступный цвет для текущей вершины
-        color = 1
-        while color in neighbor_colors:
-            color += 1
+current_color = 0
+# Повторяем окраску до тех пор, пока все вершины не будут окрашены
+while len(coloring) < len(graph):
+    current_color += 1  # Выбираем следующий цвет
+    for node in nodes_sorted_by_degree:
+        if node not in coloring:
+            adjacent_colors = {coloring[neighbor] for neighbor in graph[node] if neighbor in coloring}
+            if current_color not in adjacent_colors:
+                coloring[node] = current_color
 
-        color_map[node] = color  # Присваиваем вершине найденный цвет
-
-    return color_map
-
-# Получаем раскраску графа
-coloring = greedy_coloring(graph)
-
-# Выводим раскраску вершин графа
+# Выводим окраску вершин
+print("Результат раскраски:")
 for node, color in coloring.items():
-    print(f"Вершина {node} раскрашена в цвет {color}")
+    print(f"Вершина {node} окрашена в цвет {color}")
